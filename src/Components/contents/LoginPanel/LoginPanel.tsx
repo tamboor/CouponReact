6c270be81e18bd45e936e45b0c59b4391b19b81d
alonmintz <92505398+alonmintz@users.noreply.github.com>
2022-05-01 10:34:46 +0300

    check login button

--- src/Components/contents/LoginPanel/LoginPanel.tsx
+++ src/Components/contents/LoginPanel/LoginPanel.tsx
@@ -1,5 +1,6 @@
-import { Container, TextField, Typography } from "@mui/material";
+import { Button, Container, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useForm } from "react-hook-form";
 import "./LoginPanel.css";
 
 interface LoginProps {
@@ -11,14 +12,30 @@ interface LoginProps {
 
 function LoginPanel(props: LoginProps): JSX.Element {
   //   return <div className="LoginPanel">This is Login , {props.userType}</div>;
+
+  const { register } = useForm();
+  const onSubmit = () => {
+    console.log("submitted");
+  };
   return (
     <Container>
-      <Box>
-        <Typography>{props.userType}</Typography>
-        <TextField id="email" label="Email" variant="standard" />
-        <br />
-        <TextField id="password" label="Password" variant="standard" />
-      </Box>
+      <form onSubmit={onSubmit}>
+        <Box>
+          <Typography>{props.userType}</Typography>
+          <TextField
+            {...register("userEmail")}
+            label="Email"
+            variant="standard"
+          />
+          <br />
+          <TextField
+            {...register("userPass")}
+            label="Password"
+            variant="standard"
+          />
+          <Button type="submit">Login</Button>
+        </Box>
+      </form>
     </Container>
   );
 }
