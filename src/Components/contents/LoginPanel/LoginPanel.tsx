0b27dff791178c223940e087aec1c128c755ebf1
alonmintz <92505398+alonmintz@users.noreply.github.com>
2022-05-01 11:18:51 +0300

    Update LoginPanel.tsx

--- src/Components/contents/LoginPanel/LoginPanel.tsx
+++ src/Components/contents/LoginPanel/LoginPanel.tsx
@@ -2,6 +2,7 @@ import { Button, Container, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useForm } from "react-hook-form";
 import "./LoginPanel.css";
+import { useNavigate } from "react-router-dom";
 
 interface LoginProps {
   children?: JSX.Element;
@@ -13,29 +14,44 @@ interface LoginProps {
 function LoginPanel(props: LoginProps): JSX.Element {
   //   return <div className="LoginPanel">This is Login , {props.userType}</div>;
 
-  const { register } = useForm();
-  const onSubmit = () => {
-    console.log("submitted");
+  const navigate = useNavigate();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  // const onSubmit = (data: any) => {
+  //   console.log(data);
+  //   navigate("/");
+  // };
+  const onSubmit = (data: any) => {
+    console.log(data);
   };
+
   return (
     <Container>
-      <form onSubmit={onSubmit}>
-        <Box>
+      <Box>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <Typography>{props.userType}</Typography>
           <TextField
-            {...register("userEmail")}
+            {...register("userEmail", { required: "this is required" })}
             label="Email"
             variant="standard"
           />
           <br />
           <TextField
-            {...register("userPass")}
+            {...register("userPass", {
+              required: "this is required",
+              minLength: { value: 4, message: "Min length is 4" },
+            })}
             label="Password"
             variant="standard"
           />
           <Button type="submit">Login</Button>
-        </Box>
-      </form>
+          {/* <input type="submit" value="Login" />
+          {errors.userEmail && <span>{errors.userEmail.message}</span>} */}
+        </form>
+      </Box>
     </Container>
   );
 }
