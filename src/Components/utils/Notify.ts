b66cc117c5206e9ec688028c9ad8639035053cb4
alonmintz <92505398+alonmintz@users.noreply.github.com>
2022-05-12 10:48:48 +0300

    add customer form

---
+++ src/Components/utils/Notify.ts
@@ -0,0 +1,22 @@
+import { Notyf } from "notyf";
+
+class Notify {
+  private notification = new Notyf({
+    duration: 4000,
+    position: { x: "center", y: "top" },
+  });
+
+  public success(message: string) {
+    this.notification.success(message);
+  }
+
+  public error(message: string) {
+    console.log("error notify");
+    this.notification.error(message);
+  }
+}
+
+//create instance of our Notify Class
+const notify = new Notify();
+//expose our new shiny class to the world
+export default notify;
