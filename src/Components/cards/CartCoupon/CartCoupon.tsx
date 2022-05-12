b9cfbe8a2eef51ea876e3774a68f46b51379941a
tamboor <nirkatz20exc@gmail.com>
2022-05-12 10:49:04 +0300

    started cart

--- src/Components/cards/CartCoupon/CartCoupon.tsx
+++ src/Components/cards/CartCoupon/CartCoupon.tsx
@@ -1,6 +1,19 @@
+import axios from "axios";
+import { useEffect } from "react";
 import "./CartCoupon.css";
 
-function CartCoupon(): JSX.Element {
+interface cartItemProps{
+    id: number;
+}
+
+function CartCoupon(props:cartItemProps): JSX.Element {
+
+    useEffect(()=>{
+        axios.get().then((res)=>{}).catch(()=>{})
+    },[])
+
+
+
     return (
         <div className="CartCoupon">
 			
